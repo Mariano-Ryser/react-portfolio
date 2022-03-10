061d18ee09e07aea3e58170aaa86a1f666a6a92e
Mariano Ryser <rysermariano@gmail.com>
2022-03-10 12:32:33 +0100

    more icons

--- src/components/proyects/Proyects.jsx
+++ src/components/proyects/Proyects.jsx
@@ -22,6 +22,15 @@ import Reactt from "../../img/react.gif"
 import Linux from "../../img/linux.png"
 import Paint from "../../img/paint3d.png"
 import Window from "../../img/window.png"
+import Discord from "../../img/discord.png"
+import Git from "../../img/git.png"
+import Docker from "../../img/docker.png"
+import Php from "../../img/php.png"
+import Obs from "../../img/obs.png"
+import Pizza from "../../img/pizza.png"
+import Champi from "../../img/champi.png"
+import Bootstrap from "../../img/bootstrap.gif"
+import Sandwich from "../../img/sandwich.png"
 
 const  Proyects = () => {
 
@@ -33,15 +42,19 @@ const  Proyects = () => {
           </h1>
         </div>
         <div className="box-icons">
-          <div className="icon" style={{backgroundImage: `url(${Alienverde})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Github})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Linkedin})`}}></div>
+          <div className="icon" style={{backgroundImage: `url(${Alienverde})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Drag})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Cube})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Fo})`}}></div>
+          <div className="icon" style={{backgroundImage: `url(${Discord})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Google})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Pac})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Visual})`}}></div>
+          <div className="icon" style={{backgroundImage: `url(${Docker})`}}></div>
+          <div className="icon" style={{backgroundImage: `url(${Php})`}}></div>
+          <div className="icon" style={{backgroundImage: `url(${Git})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Snake})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Terminal})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Tic})`}}></div>
@@ -49,12 +62,17 @@ const  Proyects = () => {
           <div className="icon" style={{backgroundImage: `url(${Vue})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Bicho})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Css})`}}></div>
+          <div className="icon" style={{backgroundImage: `url(${Bootstrap})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Html})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Js})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Reactt})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Linux})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Paint})`}}></div>
           <div className="icon" style={{backgroundImage: `url(${Window})`}}></div>
+          <div className="icon" style={{backgroundImage: `url(${Obs})`}}></div>
+          <div className="icon" style={{backgroundImage: `url(${Pizza})`}}></div>
+          <div className="icon" style={{backgroundImage: `url(${Champi})`}}></div>
+          <div className="icon" style={{backgroundImage: `url(${Sandwich})`}}></div>
         </div>
     </div>
   )
