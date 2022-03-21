af132bf7a5906f34a42101acbf8c4da3fa863db6
Mariano Ryser <rysermariano@gmail.com>
2022-03-21 11:10:20 +0100

    twi

--- src/components2/presentation/Presentation.jsx
+++ src/components2/presentation/Presentation.jsx
@@ -4,6 +4,7 @@ import Foto from "../../img/Yo.png"
 import Github from "../../img/github.png"
 import Linkedin from "../../img/linkedin.png"
 import Email from "../../img/email.png"
+import Twiter from "../../img/twiter.png"
 import emailjs from '@emailjs/browser';
 
 
@@ -22,8 +23,7 @@ const Presentation = () => {
             <div className="mobil">
                 <a href="https://github.com/Mariano-Ryser"><div className="github"></div></a>
                 <a href="https://www.linkedin.com/in/mariano-ryser-073731221/"><div className="linkedin"></div></a>
-                <a href=""><div className="email"></div></a>
-                <a href=""><div className="cv"></div></a>
+                <a href=""><div className="twiter"></div></a>
             </div>
         </div>
 
